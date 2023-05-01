0511aec8775e1ebcec820884136094e8845c6cce
JOAO GABRIEL <105885425+Joaochaves0714@users.noreply.github.com>
2023-05-01 14:50:02 -0300

    Add files via upload

---
+++ 14-Buscador de Paises/assets/index.js
@@ -0,0 +1,32 @@
+
+document.getElementById('card').style.setProperty('display', 'none', 'important')
+
+function Procurar(){
+    document.getElementById('card').style.setProperty('display', 'block', 'important')
+    let pais = document.getElementById('pais').value   
+    let finalURL = `https://restcountries.com/v3.1/name/${pais}?fullText=true`
+    console.log(finalURL)
+    fetch(finalURL)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data[0])
+            // solução para o codiugo by ChatGPT
+            // document.getElementById('capital').innerHTML = data[0].capital
+            // document.getElementById('continente').innerHTML = data[0].region
+            // document.getElementById('populacao').innerHTML = data[0].population
+
+            let bandeira = document.getElementById('bandeira')
+            let nome = document.getElementById('nome')
+            let capital = document.getElementById('capital')
+            let continente = document.getElementById('continente')
+            let populacao = document.getElementById('populacao')
+
+            nome.innerHTML = pais
+            capital.innerHTML = data[0].capital[0]
+            continente.innerHTML = data[0].continents[0]
+            populacao.innerHTML = data[0].population
+            bandeira.src = data[0].flags.svg
+        })
+}
